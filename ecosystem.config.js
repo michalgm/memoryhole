9b40eecb2eb4c953f9c29a124d53b20be9c15efa
Greg <greg>
2023-11-08 13:19:36 -0800

    fix vars to serve multiple instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,20 @@
 module.exports = {
   apps: [
     {
-      name: 'serve',
-      cwd: 'current',
+      name: 'prod',
+      cwd: '/var/www/memoryhole/current',
       script: 'node_modules/.bin/rw',
       args: 'serve api',
+      instances: 'max',
+      exec_mode: 'cluster',
+      wait_ready: true,
+      listen_timeout: 10000,
+    },
+    {
+      name: 'stage',
+      cwd: '/var/www/memoryhole-stage/current',
+      script: 'node_modules/.bin/rw',
+      args: 'serve api -p 8920',
       instances: 'max',
       exec_mode: 'cluster',
       wait_ready: true,
