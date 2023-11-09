d78cd3561c171d8bba67a424b78c61a485dd3e6d
Greg <greg>
2023-11-08 21:37:55 -0800

    remove cruft

--- web/src/pages/HotlineLog/EditHotlineLogPage/EditHotlineLogPage.jsx
+++
@@ -1,7 +0,0 @@
-// import EditHotlineLogCell from 'src/components/HotlineLog/EditHotlineLogCell'
-
-const EditHotlineLogPage = ({ id }) => {
-  // return <EditHotlineLogCell id={id} />
-}
-
-export default EditHotlineLogPage
--- web/src/pages/HotlineLog/HotlineLogPage/HotlineLogPage.jsx
+++
@@ -1,7 +0,0 @@
-// import HotlineLogCell from 'src/components/HotlineLog/HotlineLogCell'
-
-const HotlineLogPage = ({ id }) => {
-  // return <HotlineLogCell id={id} />
-}
-
-export default HotlineLogPage
