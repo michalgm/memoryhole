38e4aa2078120bec2241a6355c1e565bb317561c
Greg <greg>
2023-11-08 21:29:53 -0800

    dumb fix

--- web/src/pages/HotlineLog/EditHotlineLogPage/EditHotlineLogPage.jsx
+++ web/src/pages/HotlineLog/EditHotlineLogPage/EditHotlineLogPage.jsx
@@ -1,7 +1,7 @@
 import EditHotlineLogCell from 'src/components/HotlineLog/EditHotlineLogCell'
 
 const EditHotlineLogPage = ({ id }) => {
-  return <EditHotlineLogCell id={id} />
+  // return <EditHotlineLogCell id={id} />
 }
 
 export default EditHotlineLogPage
