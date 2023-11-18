a4efb016759582c041c11acd244ca9bad0142c4a
Greg <greg>
2023-11-18 13:38:07 -0800

    trim all inputs

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -1,11 +1,44 @@
 import { authDecoder } from '@redwoodjs/auth-dbauth-api'
 import { createGraphQLHandler } from '@redwoodjs/graphql-server'
-import { db } from 'src/lib/db'
+
 import directives from 'src/directives/**/*.{js,ts}'
-import { getCurrentUser } from 'src/lib/auth'
-import { logger } from 'src/lib/logger'
 import sdls from 'src/graphql/**/*.sdl.{js,ts}'
 import services from 'src/services/**/*.{js,ts}'
+
+import { getCurrentUser } from 'src/lib/auth'
+import { db } from 'src/lib/db'
+import { logger } from 'src/lib/logger'
+
+const trimNestedStrings = (nestedObject) => {
+  if (!nestedObject || typeof nestedObject !== 'object') {
+    return
+  }
+
+  Object.keys(nestedObject).forEach((propertyKey) => {
+    if (!Object.prototype.hasOwnProperty.call(nestedObject, propertyKey)) {
+      return
+    }
+
+    const propertyValue = nestedObject[propertyKey]
+    const valueType = typeof propertyValue
+
+    if (valueType === 'string') {
+      nestedObject[propertyKey] = propertyValue.trim()
+    } else if (Array.isArray(propertyValue)) {
+      propertyValue.forEach((item, index) => {
+        if (typeof item === 'string') {
+          propertyValue[index] = item.trim()
+        }
+      })
+    } else if (valueType === 'object' && propertyValue !== null) {
+      trimNestedStrings(propertyValue)
+    }
+  })
+}
+
+const context = async ({ context }) => {
+  trimNestedStrings(context?.params?.variables?.input)
+}
 
 export const handler = createGraphQLHandler({
   authDecoder,
@@ -14,6 +47,7 @@ export const handler = createGraphQLHandler({
   directives,
   sdls,
   services,
+  context,
   // allowIntrospection: true,
   onException: () => {
     // Disconnect from your database with an unhandled exception.
