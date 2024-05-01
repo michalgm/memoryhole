33cde58f7e3dd00b44b4bfeb6b1059b2cafe3d8e
Greg <greg>
2024-04-30 23:30:14 -0700

    bump rw version add intake import scripts

---
+++ import_online_intake.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+REMOTE_HOST=arrestee-form.legalsolidaritybayarea.org
+REMOTE_USER=arrestee_intake
+REMOTE_DIR=/home/arrestee_intake/data
+LOCAL_DIR=~/arrestee_intake
+IMPORT_DIR="${LOCAL_DIR}/import"
+IMPORTED_DIR="${LOCAL_DIR}/imported"
+ERROR_DIR="${LOCAL_DIR}/error"
+
+set -e
+
+echo "### Starting intake process ###"
+for d in $LOCAL_DIR $IMPORT_DIR $IMPORTED_DIR $ERROR_DIR; do
+  mkdir -p "$d"
+done
+
+echo "Syncing files to ${IMPORT_DIR}"
+if ssh $REMOTE_USER@$REMOTE_HOST "find '${REMOTE_DIR}' -mindepth 1 -print -quit | grep -q ."; then
+  scp -Cpr $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR/* $IMPORT_DIR/
+else
+  echo "No files to transfer."
+fi
+
+# Check for .lock files in the local directory and delete them along with the corresponding files
+find "$IMPORT_DIR" -type f -name '*.lock' -print0 | while IFS= read -r -d $'\0' lockfile; do
+  echo "Found lock file: $lockfile"
+
+  # Determine the base file name by stripping '.lock'
+  basefile="${lockfile%.lock}"
+
+  # Check if the corresponding file exists
+  if [ -f "$basefile" ]; then
+    echo "Deleting file: $basefile"
+    rm -f "$basefile"
+  fi
+
+  # Delete the lock file
+  echo "Deleting lock file: $lockfile"
+  rm -f "$lockfile"
+done
+
+for file in $(ls -p $IMPORT_DIR | grep -v /); do
+  echo "Shredding remote file ${file}"
+  ssh $REMOTE_USER@$REMOTE_HOST "if [ -f ${REMOTE_DIR}/${file} ]; then shred -u ${REMOTE_DIR}/${file}; fi"
+done
+
+cd /var/www/memoryhole/current || exit
+
+for file in $(ls -p $IMPORT_DIR | grep -v /); do
+  echo "importing ${file}"
+  if yarn rw exec process_intake "${IMPORT_DIR}/${file}"; then
+    mv "${IMPORT_DIR}/${file}" "${IMPORTED_DIR}/${file}"
+  else
+    mv "${IMPORT_DIR}/${file}" "${ERROR_DIR}/${file}"
+  fi
+done
+
+echo "### Intake process complete ###"
---
+++ scripts/process_intake.js
@@ -0,0 +1,198 @@
+const fs = require('fs/promises')
+
+const axios = require('axios')
+const { wrapper } = require('axios-cookiejar-support')
+const dayjs = require('dayjs')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
+const apiHost = process.env.PUBLIC_URL
+const { set } = require('lodash')
+const { CookieJar } = require('tough-cookie')
+
+const cookieJar = new CookieJar()
+
+dayjs.extend(customParseFormat)
+
+const axiosInstance = wrapper(
+  axios.create({
+    jar: cookieJar, // Use the cookie jar
+    withCredentials: true, // Send cookies with requests
+  })
+)
+
+const fieldMap = {
+  legal_first_name: 'arrestee.first_name',
+  legal_last_name: 'arrestee.last_name',
+  preferred_name: 'arrestee.preferred_name',
+  legal_name_confidential: 'arrestee.custom_fields.legal_name_confidential',
+  preferred_pronouns: 'arrestee.pronoun',
+  birth_date: 'arrestee.dob',
+  phone_number: 'arrestee.phone_1',
+  email: 'arrestee.email',
+  address: 'arrestee.address',
+  city: 'arrestee.city',
+  state: 'arrestee.state',
+  zip: 'arrestee.zip',
+  special_needs: 'arrestee.custom_fields.risk_identifier_notes',
+  arrest_date: 'date',
+  // arrest_time: 'date',
+  arrest_city: 'arrest_city',
+  arrest_location: 'location',
+  charges: 'charges',
+  felonies: 'arrestee.custom_fields.felony_charges',
+  abuse: 'custom_fields.jail_notes',
+  prn: 'custom_fields.jail_id',
+  docket: 'custom_fields.docket_number',
+  court_date: 'custom_fields.next_court_date',
+  // courttime: 'custom_fields.next_court_date',
+  court_location: 'custom_fields.next_court_location',
+  lawyer_name: 'custom_fields.lawyer',
+  lawyer_contact_info: 'custom_fields.lawyer_contact_info',
+  notes: 'arrestee.custom_fields.arrestee_notes',
+}
+
+const query = `mutation CreateArresteeArrestMutation($input: CreateArrestInput!) {
+    createArrest(input: $input) {
+      id
+      arrestee {
+        id
+    }
+  }
+}
+`
+
+// const data = {
+//     legal_first_name: 'Fakey',
+//     legal_last_name: 'MacFakePerson',
+//     preferred_name: 'fake-o-phone',
+//     legal_name_confidential: '',
+//     preferred_pronouns: '',
+//     birth_date: '1983-01-15',
+//     phone_number: '123-234-1234',
+//     email: 'john@doe.com',
+//     address: '123 Main St',
+//     city: 'Oakland',
+//     state: 'CA',
+//     zip: '94610',
+//     special_needs: '',
+//     arrest_date: '2024-05-30',
+//     arrest_time: '01:56 AM',
+//     arrest_city: 'Oakland',
+//     arrest_location: '10th and Broadway',
+//     charges: '123 Bad cops',
+//     felonies: false,
+//     abuse: 'Ouch',
+//     prn: '12345678',
+//     docket: 'AB-123456',
+//     court_date: '2024-07-20',
+//     courttime: '9:00 AM - Wiley Courthouse',
+//     lawyer: '',
+//     notes: 'Thank you',
+//   }
+
+let id = null
+
+const combineDateTime = (date, time) => {
+  return dayjs(`${date} ${time}`, 'YYYY-MM-DD hh:mm A').format()
+}
+
+const apiRequest = async (url, data, method = 'post') => {
+  if (!id && !url.match(/auth$/)) {
+    const res = await auth()
+    id = res.data.id
+  }
+
+  const headers = {
+    'Auth-Provider': 'dbAuth',
+    Authorization: `Bearer ${id}`,
+  }
+
+  return axiosInstance({
+    url,
+    data,
+    method,
+    headers,
+  })
+    .then((response) => {
+      if (response.data.errors) {
+        throw Error('Error:', response.data.errors.join(' '))
+      }
+      return response
+    })
+    .catch((error) => {
+      if (error?.response?.data?.errors) {
+        console.error(error?.response?.data?.errors)
+      } else {
+        console.log(error?.response?.data)
+      }
+
+      throw Error('Error:', error)
+    })
+}
+
+const auth = async () => {
+  return await apiRequest(`${apiHost}/.redwood/functions/auth`, {
+    method: 'login',
+    username: process.env.IMPORT_USERNAME,
+    password: process.env.IMPORT_PASSWORD,
+  })
+}
+
+const logout = async () => {
+  return await apiRequest(`${apiHost}/.redwood/functions/auth`, {
+    method: 'logout',
+  })
+}
+
+const importRecord = async (data) => {
+  const arrest = {
+    custom_fields: {
+      custody_status: 'Out of Custody',
+      release_type: 'Unknown Released',
+      disposition: 'Open',
+      needs_review: true,
+      case_status: 'Unknown',
+    },
+    arrestee: {
+      custom_fields: {},
+    },
+  }
+  Object.keys(data).forEach((key) => {
+    if (['arrest_time', 'courttime'].includes(key)) {
+      return
+    }
+    let val = data[key]
+    if (val) {
+      const path = fieldMap[key]
+      if (path) {
+        if (key === 'arrest_date') {
+          val = combineDateTime(data.arrest_date, data.arrest_time)
+        } else if (key == 'court_date') {
+          val = combineDateTime(data.court_date, data.courttime)
+        } else if (key == 'birth_date') {
+          val = dayjs(val, 'YYYY-MM-DD').format()
+        }
+        set(arrest, path, val)
+      } else {
+        console.error(`unknown key ${key}`)
+      }
+    }
+  })
+  // console.log(arrest)
+
+  await apiRequest(`${apiHost}/.redwood/functions/graphql`, {
+    operationName: 'CreateArresteeArrestMutation',
+    query,
+    variables: { input: arrest },
+  })
+}
+
+// main()
+
+export default async ({ args }) => {
+  const file = args._[1]
+  console.log(`Importing record from ${file}`)
+  const data = await fs.readFile(file, 'utf8')
+  const json = JSON.parse(data)
+  await importRecord(json)
+  await logout()
+}
