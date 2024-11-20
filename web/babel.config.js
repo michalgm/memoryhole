0223a4b240d9edb9c2f6b06a321ecec239083aeb
Greg Michalec <greg+github@primate.net>
2024-11-20 00:05:18 -0800

    fix mui icon imports

---
+++ web/babel.config.js
@@ -0,0 +1,15 @@
+const plugins = [
+  [
+    'transform-imports',
+    {
+      '@mui/icons-material': {
+        // Use "transform" to specify how to transform the import
+        transform: '@mui/icons-material/${member}',
+        // Prevent importing the entire module
+        preventFullImport: true,
+      },
+    },
+  ],
+]
+
+module.exports = { plugins }
