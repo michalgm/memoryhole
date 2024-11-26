cc6296f982758302b56dce4bafee1df1e6782255
Greg Michalec <greg+github@primate.net>
2024-11-26 14:05:45 -0800

    syntax fix

--- web/src/pages/LoginPage/LoginPage.jsx
+++ web/src/pages/LoginPage/LoginPage.jsx
@@ -201,7 +201,7 @@ export const AuthManage = ({
             >
               {title}
             </LoadingButton>
-            {...postButtonElements}
+            {postButtonElements}
           </ModalCard>
         </FormContainer>
       </main>
