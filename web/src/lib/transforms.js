92f37f9c486c3ded4fc6386c21bf1b5beeac6e01
Greg Michalec <greg+github@primate.net>
2024-11-27 16:46:51 -0800

    fix(UI): fix handling of bad dates

--- web/src/lib/transforms.js
+++ web/src/lib/transforms.js
@@ -1,10 +1,16 @@
-import { get, set, flatMap, reduce } from 'lodash-es'
+import { flatMap, get, reduce, set } from 'lodash-es'
 
 import dayjs from '../../../api/src/lib/day'
 
 const transformers = {
-  date: (value) => (value ? dayjs(value) : null),
-  'date-time': (value) => (value ? dayjs(value) : null),
+  date: (value) => {
+    const date = dayjs(value)
+    return date.isValid() ? date : null
+  },
+  'date-time': (value) => {
+    const dateTime = dayjs(value)
+    return dateTime.isValid() ? dateTime : null
+  },
 }
 
 export const transformData = (data, fields) => {
@@ -19,13 +25,13 @@ export const transformData = (data, fields) => {
         if ((value === undefined || value == null) && params.default) {
           value = params.default
         }
-        if (value !== undefined && value !== null) {
-          const transformer = transformers[params.field_type]
-          if (transformer) {
-            value = transformer(value)
-          }
-          set(result, path, value)
+        // if ( (value !== undefined && value !== null)) {
+        const transformer = transformers[params.field_type]
+        if (transformer) {
+          value = transformer(value)
         }
+        set(result, path, value)
+        // }
         return result
       },
       {}
