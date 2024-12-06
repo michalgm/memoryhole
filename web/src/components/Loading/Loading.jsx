a7ef4304d8d0af60c5ac2378c8f871cfcecf7778
Greg Michalec <greg+github@primate.net>
2024-12-05 19:08:39 -0800

    fix(UI): fix loading component

--- web/src/components/Loading/Loading.jsx
+++ web/src/components/Loading/Loading.jsx
@@ -1,17 +1,19 @@
 import { Box, CircularProgress } from '@mui/material'
 
 const Loading = ({ size = 150, ...props }) => {
-  ;<Box
-    sx={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '50vh',
-      ...props,
-    }}
-  >
-    <CircularProgress size={size} />
-  </Box>
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '50vh',
+        ...props,
+      }}
+    >
+      <CircularProgress size={size} />
+    </Box>
+  )
 }
 
 export default Loading
