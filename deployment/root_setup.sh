2bbcf8f5144d28ae6e058d5551bf1ccead0c7601
Greg Michalec <greg+github@primate.net>
2024-12-19 01:30:55 -0800

    feat(UI): Upgraded to Material UI version 6

--- deployment/root_setup.sh
+++ deployment/root_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# shellcheck disable=all
 usermod -a -G sudo gmichalec
 passwd gmichalec
 adduser memoryhole
@@ -11,7 +11,7 @@ sudo mkdir -p /var/www/memoryhole_stage
 sudo chown memoryhole:memoryhole /var/www/memoryhole_stage
 
 mkdir /home/memoryhole/.ssh
-cp /home/gmichalec/.ssh/authorized_keys  /home/memoryhole/.ssh/
+cp /home/gmichalec/.ssh/authorized_keys /home/memoryhole/.ssh/
 chown -R memoryhole:memoryhole /home/memoryhole/.ssh/
 
 # Add Docker's official GPG key:
@@ -24,8 +24,8 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 # Add the repository to Apt sources:
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get update
 sudo apt install nginx certbot \
   docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
@@ -34,10 +34,9 @@ ufw allow "WWW Full"
 
 dd if=/dev/zero of=/swapfile.img bs=2048 count=1M
 mkswap /swapfile.img
-echo /swapfile.img swap swap sw 0 0 >> /etc/fstab
+echo /swapfile.img swap swap sw 0 0 >>/etc/fstab
 chmod 600 /swapfile.img
 swapon /swapfile.img
-
 
 sudo cp /etc/nginx/sites-enabled/memoryhole.conf /etc/nginx/sites-enabled/memoryhole-stage.conf
 sudo sed -i -e "s/memoryhole/memoryhole-stage/g" /etc/nginx/sites-enabled/memoryhole-stage.conf
