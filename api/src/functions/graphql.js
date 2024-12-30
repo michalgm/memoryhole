3a7655b3894b54e5246d55a07fab2db876348a8d
Greg Michalec <greg+github@primate.net>
2024-12-29 23:04:49 -0800

    feat(API): add User arrest_date_threshold field

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -1,13 +1,15 @@
-import { authDecoder } from '@redwoodjs/auth-dbauth-api'
+import { createAuthDecoder } from '@redwoodjs/auth-dbauth-api'
 import { createGraphQLHandler } from '@redwoodjs/graphql-server'
 
 import directives from 'src/directives/**/*.{js,ts}'
 import sdls from 'src/graphql/**/*.sdl.{js,ts}'
 import services from 'src/services/**/*.{js,ts}'
 
-import { getCurrentUser } from 'src/lib/auth'
+import { cookieName, getCurrentUser } from 'src/lib/auth'
 import { db } from 'src/lib/db'
 import { logger } from 'src/lib/logger'
+
+const authDecoder = createAuthDecoder(cookieName)
 
 const trimNestedStrings = (nestedObject) => {
   if (!nestedObject || typeof nestedObject !== 'object') {
@@ -48,7 +50,8 @@ export const handler = createGraphQLHandler({
   sdls,
   services,
   context,
-  // allowIntrospection: true,
+  allowIntrospection: process.env.NODE_ENV === 'development',
+  allowGraphiQL: process.env.NODE_ENV === 'development',
   onException: () => {
     // Disconnect from your database with an unhandled exception.
     db.$disconnect()
