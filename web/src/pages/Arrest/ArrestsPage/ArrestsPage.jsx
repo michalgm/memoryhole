3556b7b9d158a19c9c49673fd907eea8ea437bc6
Greg Michalec <greg+github@primate.net>
2025-01-01 20:30:30 -0800

    refactor(UI): remove arrestsCell and merge into arrestsPage (to prevent duplicate lookups)

--- web/src/pages/Arrest/ArrestsPage/ArrestsPage.jsx
+++ web/src/pages/Arrest/ArrestsPage/ArrestsPage.jsx
@@ -1,5 +1,80 @@
-import ArrestsCell from 'src/components/ArrestsCell/ArrestsCell'
+// import ArrestsCell from 'src/components/ArrestsCell/ArrestsCell'
+import { useState } from 'react'
+
+import { Grid2, Typography } from '@mui/material'
+import { Stack } from '@mui/system'
+import dayjs from 'dayjs'
+import { useConfirm } from 'material-ui-confirm'
+
+import { routes } from '@redwoodjs/router'
+import { useMutation, useQuery } from '@redwoodjs/web'
+
+import DataTable from 'src/components/DataTable/DataTable'
+import BulkUpdateModal from 'src/components/utils/BulkUpdateModal'
+import Link from 'src/components/utils/Link'
+import { useDisplayError, useSnackbar } from 'src/components/utils/SnackBar'
 import { useApp } from 'src/lib/AppContext'
+import { schema } from 'src/lib/FieldSchemas'
+
+export const QUERY = gql`
+  query ArrestsQuery($filters: [GenericFilterInput]) {
+    arrests: filterArrests(filters: $filters) {
+      ...ArrestFields
+    }
+  }
+`
+const BULK_DELETE_ARRESTS = gql`
+  mutation BulkDeleteArrests($ids: [Int]!) {
+    bulkDeleteArrests(ids: $ids) {
+      count
+    }
+  }
+`
+
+const BULK_UPDATE_ARRESTS = gql`
+  mutation BulkUpdateArrests($ids: [Int]!, $input: UpdateArrestInput!) {
+    bulkUpdateArrests(ids: $ids, input: $input) {
+      count
+    }
+  }
+`
+
+const tableProps = {
+  enableColumnFilterModes: true,
+  // muiTableContainerProps: { sx: { maxHeight: 'calc(100vh - 283px)' } },
+  enableColumnPinning: true,
+  enableRowSelection: true,
+  enableColumnOrdering: true,
+  selectAllMode: 'all',
+
+  initialState: {
+    showGlobalFilter: true,
+    sorting: [{ id: 'date', desc: true }],
+    columnPinning: {
+      left: ['mrt-row-select', 'arrestee.display_field'],
+    },
+  },
+}
+
+const displayColumns = [
+  'date',
+  'custom_fields.custody_status',
+  'custom_fields.disposition',
+  'custom_fields.release_type',
+]
+
+const preColumns = [
+  {
+    accessorKey: 'arrestee.display_field',
+    id: 'arrestee.display_field',
+    header: 'Name',
+    Cell: ({ row, renderedCellValue }) => (
+      <Link color="secondary" to={routes.arrest({ id: row.original.id })}>
+        {renderedCellValue}
+      </Link>
+    ),
+  },
+]
 
 const ArrestsPage = () => {
   const { currentAction } = useApp()
@@ -11,9 +86,115 @@ const ArrestsPage = () => {
       value: currentAction.id,
     })
   }
+  const [bulkUpdateRows, setBulkUpdateRows] = useState(null)
+  const { openSnackbar } = useSnackbar()
+
+  const displayError = useDisplayError()
+
+  const confirm = useConfirm()
+  const {
+    data: responseData,
+    loading,
+    refetch,
+  } = useQuery(QUERY, { variables: { filters } })
+
+  const [bulkDeleteArrests] = useMutation(BULK_DELETE_ARRESTS, {
+    onCompleted: () => {
+      refetch()
+    },
+  })
+
+  const data = responseData?.arrests
+
+  const bulkUpdate = (table) => {
+    setBulkUpdateRows(table.getSelectedRowModel().rows)
+  }
+
+  const bulkDelete = async (table) => {
+    const { rows } = table.getSelectedRowModel()
+    const ids = rows.map(({ id }) => id)
+    const list = rows.map((row) => {
+      return [
+        <Grid2 key={`${row.id}-name`} size={8}>
+          {`${row.original?.arrestee?.display_field}`}
+        </Grid2>,
+        <Grid2 key={`${row.id}-date`} size={4}>
+          {`${dayjs(row.original?.date).format('L hh:mm A')}`}
+        </Grid2>,
+      ]
+    })
+    await confirm({
+      content: (
+        <>
+          Are you sure you want to delete the following arrests?
+          <Grid2 container spacing={2} sx={{ mt: 2 }}>
+            <Grid2 key={'name'} size={8}>
+              <b>Name</b>
+            </Grid2>
+            <Grid2 key={'date'} size={4}>
+              <b>Arrest Date</b>
+            </Grid2>
+
+            {list.flat()}
+          </Grid2>
+        </>
+      ),
+    })
+    try {
+      const {
+        data: {
+          bulkDeleteArrests: { count },
+        },
+      } = await bulkDeleteArrests({ variables: { ids } })
+      openSnackbar(`${count} arrest records deleted`)
+      // closeModal()
+      table.resetRowSelection()
+      refetch()
+    } catch (error) {
+      displayError(error)
+    }
+  }
   return (
     <>
-      <ArrestsCell filters={filters} />
+      <Stack spacing={0} direction="column">
+        <DataTable
+          data={data}
+          schema={schema}
+          displayColumns={displayColumns}
+          tableProps={tableProps}
+          refetch={refetch}
+          preColumns={preColumns}
+          bulkUpdate={bulkUpdate}
+          bulkDelete={bulkDelete}
+          manageViews
+          loading={loading}
+          type="arrestees"
+          name="arrest"
+          persistState
+          footerNotes={
+            <Stack spacing={0} direction="column">
+              <Typography variant="caption">
+                If a preferred name is provided, the legal first name appears in
+                parentheses.
+              </Typography>
+              <Typography variant="caption">
+                * indicates the displayed name is the preferred name, and the
+                legal name is confidential.
+              </Typography>
+            </Stack>
+          }
+        />
+      </Stack>
+      <BulkUpdateModal
+        bulkUpdateRows={bulkUpdateRows}
+        setBulkUpdateRows={setBulkUpdateRows}
+        mutation={BULK_UPDATE_ARRESTS}
+        schema={schema}
+        name="arrest"
+        onSuccess={() => {
+          refetch()
+        }}
+      />
     </>
   )
 }
--- web/src/pages/Arrest/ArrestsPage/ArrestsPage.test.jsx
+++ web/src/pages/Arrest/ArrestsPage/ArrestsPage.test.jsx
@@ -1,25 +1,53 @@
-import { render } from '@redwoodjs/testing/web'
+import { screen, waitFor } from '@redwoodjs/testing/web'
 
+import { SnackBarProvider } from 'src/components/utils/SnackBar'
 import AppProvider from 'src/lib/AppContext'
+import { render } from 'src/setupTests'
 
 import ArrestsPage from './ArrestsPage'
-jest.mock('@redwoodjs/router', () => ({
-  ...jest.requireActual('@redwoodjs/router'),
-  routes: {
-    home: jest.fn(() => `/`),
-  },
-}))
-//   Improve this test with help from the Redwood Testing Doc:
-//   https://redwoodjs.com/docs/testing#testing-pages-layouts
+import { standard } from './ArrestsPage.mock'
 
 describe('ArrestsPage', () => {
-  it('renders successfully', () => {
+  beforeEach(() => {
+    mockGraphQLQuery('FindTableViews', () => {
+      return {
+        tableViews: [
+          {
+            id: '1',
+            name: 'Test View',
+            updated_by_id: 1,
+            updated_at: '2023-11-10T12:00:00Z',
+            created_at: '2023-11-10T12:00:00Z',
+            created_by_id: 1,
+            state: '',
+            type: 'table',
+          },
+        ],
+      }
+    })
+    mockGraphQLQuery('ArrestsQuery', () => {
+      return standard
+    })
+  })
+
+  it('renders successfully', async () => {
     expect(() => {
       render(
         <AppProvider>
-          <ArrestsPage />
+          <SnackBarProvider>
+            <ArrestsPage />
+          </SnackBarProvider>
         </AppProvider>
       )
     }).not.toThrow()
+
+    const firstElement = await waitFor(() =>
+      screen.getByText(standard.arrests[0].arrestee.display_field)
+    )
+    expect(firstElement).toBeInTheDocument()
+    const secondElement = screen.getByText(
+      standard.arrests[1].arrestee.display_field
+    )
+    expect(secondElement).toBeInTheDocument()
   })
 })
