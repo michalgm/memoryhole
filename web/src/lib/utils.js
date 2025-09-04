1faea32ee43df7bbc07e9407e6159a88b134bb60
Greg Michalec <greg+github@primate.net>
2025-09-04 13:01:19 -0700

    refactor: update ArrestChooser and QuickSearch components to utilize displayItemProps for improved rendering

--- web/src/lib/utils.js
+++ web/src/lib/utils.js
@@ -1,3 +1,7 @@
+import { Person } from '@mui/icons-material'
+import { Chip, Tooltip, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import dayjs from 'dayjs'
 import { renderToStaticMarkup } from 'react-dom/server'
 
 export const asyncDebounce = (fn, wait) => {
@@ -20,4 +24,53 @@ export const convertSvgToDataUrl = (Icon, color = 'white') => {
   return `url('data:image/svg+xml,${renderToStaticMarkup(
     <Icon style={{ fill: color }} />
   ).replace('<svg ', '<svg xmlns="http://www.w3.org/2000/svg" ')}')`
+}
+
+export const displayItemProps = ({ item = {}, type = 'arrest' }) => {
+  let title = ''
+  let subtitle = ''
+  let icon = ''
+
+  if (type === 'arrest') {
+    const { date, arrest_city } = item || {}
+    title = item?.arrestee?.search_display_field || 'Unknown'
+    const separator = date && arrest_city ? ' | ' : ''
+    subtitle = `${date ? dayjs(date).format('L') : ''}${separator}${arrest_city ? arrest_city : ''}`
+    icon = <Person />
+  }
+
+  return { title, subtitle, icon }
+}
+
+export const displayItem = ({
+  item = {},
+  type = 'arrest',
+  showIcon = true,
+  titleProps = {},
+  subtitleProps = {},
+  ...props
+}) => {
+  const { title, subtitle, icon } = displayItemProps({ item, type })
+  return (
+    <Tooltip title={subtitle} {...subtitleProps}>
+      <Chip
+        {...props}
+        size="small"
+        component={'span'}
+        icon={showIcon && icon ? icon : null}
+        label={
+          <Box component="span">
+            <Typography
+              component="span"
+              variant="body2"
+              sx={{ fontWeight: 'medium' }}
+              {...titleProps}
+            >
+              {title}
+            </Typography>
+          </Box>
+        }
+      />
+    </Tooltip>
+  )
 }
