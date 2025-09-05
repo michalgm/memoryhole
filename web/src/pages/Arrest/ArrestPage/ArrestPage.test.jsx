895a61b4f9c7bdfa5f449ecd62db4a4974b5113f
Greg Michalec <greg+github@primate.net>
2025-09-04 22:12:25 -0700

    fix(useFormManager): pass formState.isDirty to resetForm for improved state handling
    fix(ArrestPage.test): refactor tests to use renderArrestPage helper and enhance assertions
    
    fix(browserMocks): update matchMedia mock to reflect pointer precision

--- web/src/pages/Arrest/ArrestPage/ArrestPage.test.jsx
+++ web/src/pages/Arrest/ArrestPage/ArrestPage.test.jsx
@@ -1,6 +1,9 @@
+import userEvent from '@testing-library/user-event'
 import dayjs from 'dayjs'
+import { ConfirmProvider } from 'material-ui-confirm'
 
-import { mockGraphQLQuery } from '@redwoodjs/testing/web'
+import { navigate } from '@redwoodjs/router'
+import { mockGraphQLQuery, screen, waitFor } from '@redwoodjs/testing/web'
 
 import { SnackBarProvider } from 'src/components/utils/SnackBar'
 import AppProvider from 'src/lib/AppContext'
@@ -8,7 +11,23 @@ import { render } from 'src/setupTests'
 
 import ArrestPage from './ArrestPage'
 
+const renderArrestPage = (props = {}) => {
+  return render(
+    <AppProvider>
+      <SnackBarProvider>
+        <ConfirmProvider>
+          <ArrestPage {...props} />
+        </ConfirmProvider>
+      </SnackBarProvider>
+    </AppProvider>
+  )
+}
+const user = userEvent.setup()
+
+const createMutationSpy = jest.fn()
+
 describe('ArrestPage', () => {
+  mockGraphQLQuery('searchActions', () => ({ searchActions: [] }))
   mockGraphQLQuery('EditArrestById', (variables) => {
     return {
       arrest: {
@@ -19,8 +38,11 @@ describe('ArrestPage', () => {
         updated_by: { name: 'Test User' },
         date: dayjs().format('YYYY-MM-DD'),
         jurisdiction: 'Alameda',
+        city: 'Oakland',
         arrestee: {
           id: 1,
+          first_name: 'Test',
+          last_name: 'Arrestee',
           display_field: 'Test Arrestee',
         },
       },
@@ -28,13 +50,87 @@ describe('ArrestPage', () => {
   })
   it('renders successfully', async () => {
     expect(() => {
-      render(
-        <AppProvider>
-          <SnackBarProvider>
-            <ArrestPage id={1} />
-          </SnackBarProvider>
-        </AppProvider>
-      )
+      renderArrestPage({ id: 1 })
     }).not.toThrow()
+    await waitFor(() => {
+      expect(screen.getByText('Save Arrest')).toBeInTheDocument()
+      expect(screen.getByText('Delete Arrest')).toBeInTheDocument()
+    })
+    const nameField = await screen.findByLabelText('Legal First Name')
+    expect(nameField).toHaveValue('Test')
+  })
+})
+
+describe('ArrestPage - Create', () => {
+  mockGraphQLMutation('CreateArrestMutation', (variables) => {
+    createMutationSpy(variables) // Call the spy with the variables
+
+    return {
+      createArrest: {
+        id: 123,
+        ...variables.input,
+      },
+    }
+  })
+  it('renders create successfully', async () => {
+    expect(() => {
+      renderArrestPage()
+    }).not.toThrow()
+
+    await waitFor(() => {
+      expect(screen.getByText('Save Arrest')).toBeInTheDocument()
+    })
+    expect(screen.queryByText('Delete Arrest')).not.toBeInTheDocument()
+  })
+
+  it('runs save successfully', async () => {
+    renderArrestPage({})
+
+    await waitFor(() => {
+      expect(screen.getByText('Save Arrest')).toBeInTheDocument()
+    })
+    expect(screen.queryByText('Delete Arrest')).not.toBeInTheDocument()
+    const nameField = await screen.findByLabelText('Legal First Name')
+    const dateField = await screen.findByLabelText('Arrest Date *')
+    const cityField = await screen.findByLabelText('Arrest City *')
+
+    await user.click(nameField)
+    await user.type(nameField, 'Jane')
+
+    await user.type(cityField, 'Oakland')
+    await user.keyboard('{ArrowDown}') // Navigate to first option
+    await user.keyboard('{Enter}')
+
+    await user.click(dateField)
+    await user.keyboard('{Tab}') // Move to time part if datetime picker
+    await user.type(dateField, '081020250200A')
+    await user.keyboard('{Tab}') // Move to time part if datetime picker
+
+    // expect(cityField).toHaveValue('Oakland')
+    // expect(dateField).toHaveValue('08/10/2025 02:00 AM')
+
+    const saveButton = screen.getByRole('button', { name: /save arrest/i })
+
+    await waitFor(() => expect(saveButton).toBeEnabled())
+    await user.click(saveButton)
+
+    await waitFor(async () => {
+      expect(createMutationSpy).toHaveBeenCalledTimes(1)
+    })
+    await waitFor(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 300))
+    })
+    await waitFor(() => {
+      navigate(`/foo`)
+    })
+
+    await waitFor(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 300))
+    })
+
+    const dialog = screen.queryByText(/are you sure/i, {
+      selector: '.MuiTypography-root',
+    })
+    expect(dialog).not.toBeInTheDocument()
   })
 })
