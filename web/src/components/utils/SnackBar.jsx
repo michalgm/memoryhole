50964699a9a8c83540305290c669b96ca8382b19
Greg Michalec <greg+github@primate.net>
2025-09-08 21:46:30 -0700

    feat(ui): improve error display

--- web/src/components/utils/SnackBar.jsx
+++ web/src/components/utils/SnackBar.jsx
@@ -15,6 +15,7 @@ import {
   AlertTitle,
   List,
   ListItem,
+  ListItemText,
   Snackbar,
   Typography,
 } from '@mui/material'
@@ -33,13 +34,11 @@ export const useSnackbar = () => {
   return useContext(SnackbarContext)
 }
 
-const errorList = (errors) => {
-  const errorMessages = errors.map((err, index) => {
-    const message = [err.message, err.extensions?.originalError?.message].join(
-      ' '
-    )
+const errorsDetails = (errors) => {
+  const errorMessage = (stack, index) => {
     return (
       <ListItem key={`error-${index}`}>
+        <ListItemText primary="Stack trace:" />
         <Typography
           component="pre"
           variant="body2"
@@ -48,18 +47,39 @@ const errorList = (errors) => {
             whiteSpace: 'pre-wrap', // Preserve formatting and wrap long lines
           }}
         >
-          {message}
+          {stack}
         </Typography>
       </ListItem>
     )
-  })
-  return <List dense>{errorMessages}</List>
+  }
+
+  const stacks = errors.reduce((acc, err, index) => {
+    const stack = err.stack || err.extensions?.originalError?.stack
+    if (stack && !acc.includes(stack)) {
+      acc.push(errorMessage(stack, index))
+    }
+    return acc
+  }, [])
+  if (stacks.length === 0) {
+    return null
+  }
+  return (
+    <Accordion sx={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+        <Typography variant="body2">Details</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <List dense>{stacks}</List>
+      </AccordionDetails>
+    </Accordion>
+  )
 }
 
 const formatError = (error) => {
   if (React.isValidElement(error)) {
     return error
   }
+
   if (
     error?.graphQLErrors?.[0]?.extensions?.originalError?.message ===
       INVALID_AUTH_HEADER_ERROR ||
@@ -68,26 +88,52 @@ const formatError = (error) => {
   ) {
     return
   }
-  const message =
+
+  let rootMessage =
     typeof error === 'string'
       ? error
-      : error?.message || error?.[0]?.message || 'Unknown error'
+      : error?.message ||
+        error?.graphQLErrors?.[0]?.message ||
+        error?.[0]?.message ||
+        'Unknown error'
 
   const errors = [
     ...(error.graphQLErrors || []),
     ...(error.networkError?.result?.errors || []),
   ]
+  if (error?.[0]?.extensions?.code === 'BAD_USER_INPUT') {
+    rootMessage = 'Errors prevented this form from being saved:'
+  }
   const errorMessage = (
     <>
       <AlertTitle>Error</AlertTitle>
-      <Typography gutterBottom>{message || 'Unknown error'}</Typography>
+      <Typography gutterBottom>{rootMessage}</Typography>
       {errors.length > 0 && (
-        <Accordion sx={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
-          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography variant="body2">Details</Typography>
-          </AccordionSummary>
-          <AccordionDetails>{errorList(errors)}</AccordionDetails>
-        </Accordion>
+        <>
+          <ul>
+            {errors.map((error, index) => {
+              let message = error.message
+              let secondaryMessage = error.extensions?.originalError?.message
+
+              if (message === rootMessage) {
+                if (!secondaryMessage) {
+                  return null
+                } else {
+                  message = secondaryMessage
+                }
+              }
+              return (
+                <li key={index}>
+                  {message}{' '}
+                  {secondaryMessage &&
+                    secondaryMessage != message &&
+                    `(${secondaryMessage})`}
+                </li>
+              )
+            })}
+          </ul>
+          {errorsDetails(errors)}
+        </>
       )}
     </>
   )
