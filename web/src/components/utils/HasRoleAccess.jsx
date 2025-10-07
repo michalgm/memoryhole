975c65da9b44d0dc6cf0cdff0fb0292c546aae49
Greg Michalec <greg+github@primate.net>
2025-10-07 02:11:15 -0700

    fix: filter documents based on access role

--- web/src/components/utils/HasRoleAccess.jsx
+++ web/src/components/utils/HasRoleAccess.jsx
@@ -1,19 +1,26 @@
 import { ROLE_LEVELS } from 'src/../../api/src/config'
 import { useAuth } from 'src/auth'
 
-const HasRoleAccess = ({ children, requiredRole }) => {
-  const { currentUser } = useAuth()
-  const userRoleLevel = ROLE_LEVELS.indexOf(currentUser?.roles?.[0])
+export const validateRoleLevel = (requiredRole, userRole) => {
+  const userRoleLevel = ROLE_LEVELS.indexOf(userRole)
   const requiredRoleLevel = ROLE_LEVELS.indexOf(requiredRole)
-
   if (requiredRole && requiredRoleLevel < 0) {
     console.warn(`Unknown required role: ${requiredRole}`)
-    return null
+    return false
   }
   if (userRoleLevel < requiredRoleLevel) {
-    return null
+    return false
   }
 
-  return children
+  return true
+}
+
+const HasRoleAccess = ({ children, requiredRole }) => {
+  const { currentUser } = useAuth()
+  const userRole = currentUser?.roles?.[0]
+
+  if (validateRoleLevel(requiredRole, userRole)) {
+    return children
+  }
 }
 export default HasRoleAccess
