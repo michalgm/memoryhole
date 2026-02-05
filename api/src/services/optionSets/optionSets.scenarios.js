43b46c3617789bb33298723afe3964b97a41985a
Greg Michalec <greg+github@primate.net>
2026-02-05 00:38:26 -0800

    feat(optionSets): implement validation for deleting and renaming option values in use

--- api/src/services/optionSets/optionSets.scenarios.js
+++ api/src/services/optionSets/optionSets.scenarios.js
@@ -1,6 +1,37 @@
 export const standard = defineScenario({
   optionSet: {
+    logType: {
+      data: {
+        name: 'log_type',
+        values: {
+          create: [
+            { value: 'MyLogType', label: 'My Log Type', order: 1 },
+            { value: 'OtherLogType', label: 'Other', order: 2 },
+          ],
+        },
+      },
+    },
+    jailPopulation: {
+      data: {
+        name: 'jail_population',
+        values: {
+          create: [
+            { value: 'Unknown', label: 'Unknown', order: 1 },
+            { value: 'General', label: 'General', order: 2 },
+          ],
+        },
+      },
+    },
     one: { data: { name: 'String4627362' } },
     two: { data: { name: 'String9299623' } },
+  },
+  log: {
+    one: {
+      data: {
+        time: '2023-01-01T00:00:00Z',
+        type: 'MyLogType',
+        notes: 'test',
+      },
+    },
   },
 })
